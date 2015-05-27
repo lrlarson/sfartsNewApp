e279a925fb3b297995aa3c504c8804e5c6db6cb7
lrlarson <lrlarson@mac.com>
2015-05-27 15:01:35 -0400

    version 1.0
    Signed-off-by: lrlarson <lrlarson@mac.com>

---
+++ platforms/android/ant-build/CordovaApp-release-unsigned.apk.d
@@ -0,0 +1,4 @@
+/applications/coldfusion9/wwwroot/SFArts-Express/platforms/android/ant-build/CordovaApp-release-unsigned.apk : \
+/applications/coldfusion9/wwwroot/SFArts-Express/platforms/android/ant-build/CordovaApp.ap_ \
+/applications/coldfusion9/wwwroot/SFArts-Express/platforms/android/ant-build/classes.dex \
+/Applications/ColdFusion9/wwwroot/SFArts-Express/platforms/android/CordovaLib/ant-build/classes.jar \
