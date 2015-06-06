d256623d9a4a129d0d1a353e6276ee794a6e6109
lrlarson <lrlarson@mac.com>
2015-06-06 13:25:09 -0400

    First Published Edition
    Signed-off-by: lrlarson <lrlarson@mac.com>

--- platforms/android/ant-build/AndroidManifest.xml.d
+++ platforms/android/ant-build/AndroidManifest.xml.d
@@ -1,2 +1,2 @@
-/applications/coldfusion9/wwwroot/SFArts-Express/platforms/android/ant-build/AndroidManifest.xml : \
-/applications/coldfusion9/wwwroot/SFArts-Express/platforms/android/AndroidManifest.xml \
+/Applications/ColdFusion9/wwwroot/SFArts-Express/platforms/android/ant-build/AndroidManifest.xml : \
+/Applications/ColdFusion9/wwwroot/SFArts-Express/platforms/android/AndroidManifest.xml \
